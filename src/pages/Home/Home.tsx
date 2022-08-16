ab57edcbf2f56fa64b8fc5dfc53940152deba755
paulo uilian <uilianlbarros@gmail.com>
2022-08-16 11:49:27 -0300

    implement alert page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,15 @@
 import styled from 'styled-components';
+import AccessDenied from '../../components/AccessDenied/AccessDenied';
 import InstructorPlan from '../../components/InstructorPlans/InstructorPlans';
+import useAuth from '../../hooks/useAuth';
 
 function Home() {
+	const { userInfo } = useAuth();
+
+	if (!userInfo?.isInstructor) {
+		return <AccessDenied />;
+	}
+
 	return (
 		<HomeContainer>
 			<InstructorPlan />
